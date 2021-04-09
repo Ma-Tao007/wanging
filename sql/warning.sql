aa985cb4b0a16d0ee5761c7763103f846f9293eb
maker <5134000+mt-GT@user.noreply.gitee.com>
2021-04-09 14:37:00 +0800

    数据库更新

--- sql/warning.sql
+++ sql/warning.sql
@@ -11,32 +11,32 @@
  Target Server Version : 80011
  File Encoding         : 65001
 
- Date: 07/04/2021 13:39:52
+ Date: 09/04/2021 14:36:43
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for drug
+-- Table structure for grade
 -- ----------------------------
-DROP TABLE IF EXISTS `drug`;
-CREATE TABLE `drug`  (
+DROP TABLE IF EXISTS `grade`;
+CREATE TABLE `grade`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `drugname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `price` decimal(10, 2) NULL DEFAULT NULL,
-  `num` int(11) NULL DEFAULT NULL,
-  `inputtime` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `sno` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `sname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `cname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `credit` int(255) NULL DEFAULT NULL,
+  `grade` int(255) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of drug
+-- Records of grade
 -- ----------------------------
-INSERT INTO `drug` VALUES (1, '福尔马林333', 28.00, 10, '2020-04-20 12:12:30');
-INSERT INTO `drug` VALUES (2, '阿莫西林', 44.00, 20, '2020-05-12 10:17:51');
-INSERT INTO `drug` VALUES (3, '福尔马林s', 123.00, 77, '2020-05-12 10:29:29');
-INSERT INTO `drug` VALUES (4, '板蓝根', 15.00, 100, '2020-05-12 10:29:46');
+INSERT INTO `grade` VALUES (2, '02', 'maker3', 'Java Javascript', 3, 78);
+INSERT INTO `grade` VALUES (3, '01', 'maker3', 'java', 3, 90);
+INSERT INTO `grade` VALUES (4, '01', 'maker3', 'c++', 2, 53);
 
 -- ----------------------------
 -- Table structure for student
@@ -55,6 +55,9 @@ CREATE TABLE `student`  (
 -- ----------------------------
 -- Records of student
 -- ----------------------------
+INSERT INTO `student` VALUES ('01', 'maker3', '男', 23, '软件', '111111', NULL);
+INSERT INTO `student` VALUES ('02', 'maker2', '男', 21, '软件', '111111', NULL);
+INSERT INTO `student` VALUES ('03', 'maker', '男', 31, '软件', '111111', NULL);
 
 -- ----------------------------
 -- Table structure for user
@@ -65,7 +68,7 @@ CREATE TABLE `user`  (
   `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
